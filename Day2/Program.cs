8667bb73a8a6b59bad4c21b61e91d18d518f1f36
Johan Bastiaan <johan.bastiaan@rtl.nl>
2021-12-02 20:28:12 +0100

    Day2 part1

---
+++ Day2/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Data;
+using System.Linq;
+
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+var inputs = lines.Select(l =>
+{
+    var parts = l.Split(' ');
+    return (Command: parts[0], Value: int.Parse(parts[1]));
+});
+
+int hPos = 0;
+int vPos = 0;
+foreach (var input in inputs)
+{
+    switch (input.Command)
+    {
+        case "forward":
+            hPos += input.Value;
+            continue;
+        case "up":
+            vPos -= input.Value;
+            continue;
+        case "down":
+            vPos += input.Value;
+            continue;
+    }
+}
+
+var answer = hPos * vPos;
+Console.WriteLine(answer);
+
+
