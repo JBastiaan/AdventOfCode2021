dc692c902d4571a8ca9d885c0c9b169216814fa8
Johan Bastiaan <johan.bastiaan@rtl.nl>
2021-12-08 18:27:24 +0100

    Day8 part 1

---
+++ Day8/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using Day8;
+
+var lines = System.IO.File.ReadLines("input.txt");
+
+int[] uniqueSegmentCounts = new[]
+{
+    SegmentCount.D1,
+    SegmentCount.D4,
+    SegmentCount.D7,
+    SegmentCount.D8
+};
+ 
+var result = lines
+    .Select(s => s.Split('|').Skip(1).First())
+    .SelectMany(s => s.Split())
+    .Where(s => uniqueSegmentCounts.Contains(s.Length))
+    .Count();
+
+Console.WriteLine(result);
---
+++ Day8/SegmentCount.cs
@@ -0,0 +1,10 @@
+namespace Day8
+{
+    public static class SegmentCount
+    {
+        public const int D1 = 2;
+        public const int D4 = 4;
+        public const int D7 = 3;
+        public const int D8 = 7;
+    }
+}
